44d91a71e09528426355a5c9d52b3a13bf947e2c
nosami <jasonimison@gmail.com>
2018-05-15 19:32:26 +0100

    Add digit buttons

--- Elmish.Calculator/Calculator.fs
+++ Elmish.Calculator/Calculator.fs
@@ -15,6 +15,13 @@ type Model =
       operand2: double
       operator: Operator option }
 
+/// Represents a calculator button
+type Msg =
+    | Operator of Operator
+    | Digit of int
+    | Equals
+    | Clear
+
 type App() as app =
     inherit Application()
 
@@ -25,7 +32,25 @@ type App() as app =
         | _ -> model
 
     let view model dispatch =
-        Xaml.ContentPage()
+        let mkNumberButton number row column =
+            Xaml.Button(text = string number, command=(fun () -> dispatch (Digit number)))
+                .GridRow(row)
+                .GridColumn(column)
+                .BackgroundColor(Color.White)
+                .TextColor(Color.Black)
+                .FontSize("36px")
+
+        Xaml.ContentPage(
+            Xaml.Grid(rowdefs=[ "2*"; "*"; "*"; "*"; "*"; "*"; "*" ], coldefs=[ "*"; "*"; "*"; "*" ],
+                children=[
+                    Xaml.Label(fontSize = "48px", fontAttributes = FontAttributes.Bold, backgroundColor = Color.Black, textColor = Color.White, horizontalTextAlignment = TextAlignment.End, verticalTextAlignment = TextAlignment.Center, gridColumnSpan = 4)
+                    mkNumberButton 7 1 0; mkNumberButton 8 1 1; mkNumberButton 9 1 2
+                    mkNumberButton 4 2 0; mkNumberButton 5 2 1; mkNumberButton 6 2 2
+                    mkNumberButton 1 3 0; mkNumberButton 2 3 1; mkNumberButton 3 3 2
+                    (mkNumberButton 0 4 0).GridColumnSpan(3)
+                ]
+            )
+        )
 
     let runner = 
         Program.mkSimple init update view
